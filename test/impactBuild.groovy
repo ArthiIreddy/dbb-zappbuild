d6fc0488a75423d6b963af8ba5e254b5adc0c305
Arthi Ireddy <arthi.ireddy@ibm.com>
2021-02-18 09:01:05 -0500

    added print statements

--- test/impactBuild.groovy
+++ test/impactBuild.groovy
@@ -4,7 +4,7 @@ import com.ibm.dbb.*
 import com.ibm.dbb.build.*
 
 @Field BuildProperties properties = BuildProperties.getInstance()
-println "********Executing impact build using these build properties =\n ${properties.list()} \n"
+println "/////********Executing impact build using these build properties\n ${properties.list()} \n"
 /******************************************************************************************
 1. Edits the file for incremental build 
 2. Runs a incremental/impact build based on file changed
